5b7812f3af996022788eb78f0537378f87867b37
Kelvin Kapaya <kalvinkapu@gmail.com>
2023-06-24 10:27:49 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,27 @@
+// const input = document.getElementById('input');
+// function addingEventListener() {
+// const input = document.getElementById('input');
+
+//   alert('I was clicked!');
+// };
+// input.addingEventListener('click', clickAlert);
+
+// function clickAlert() {
+//     alert('I was clicked!');
+//   }
+  
+//   input.addEventListener('click', clickAlert);
+
+
 function addingEventListener() {
+  const input = document.getElementById('input');
+
+  function clickAlert() {
+    alert('I was clicked!');
+  }
+
+  input.addEventListener('click', clickAlert);
 }
+
+// Call the addingEventListener function to bind the event listener
+addingEventListener();
